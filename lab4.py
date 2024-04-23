2caa303403c810d2bd0b589de1cd58591fa2972d
Vasilich563 <yackub563@mail.ru>
2024-04-23 23:17:48 +0300

    upload lab4

---
+++ lab4.py
@@ -0,0 +1,387 @@
+import random
+
+from matplotlib import pyplot as plt
+import numpy as np
+import tkinter as tk
+from tkinter import ttk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from random import randint
+
+
+from typing import NamedTuple
+
+
+class Dot:
+    def __init__(self, x, y, z):
+        self.x_coordinate: float = x
+        self.y_coordinate: float = y
+        self.z_coordinate: float = z
+
+
+class Figure:
+    def __init__(self, dots):
+        self.dots = dots
+        self.update_midpoint()
+
+    def update_midpoint(self):
+        num_dots = len(self.dots)
+        sum_x = sum(dot.x_coordinate for dot in self.dots)
+        sum_y = sum(dot.y_coordinate for dot in self.dots)
+        sum_z = sum(dot.z_coordinate for dot in self.dots)
+        mid_x = sum_x / num_dots
+        mid_y = sum_y / num_dots
+        mid_z = sum_z / num_dots
+        self.midpoint = Dot(mid_x, mid_y, mid_z)
+
+    def rotate(self, angle, axis):
+        rotation_matrix = self.get_rotation_matrix(angle, axis)
+        for dot in self.dots:
+            dot.x_coordinate, dot.y_coordinate, dot.z_coordinate = np.dot(
+                rotation_matrix, [dot.x_coordinate, dot.y_coordinate, dot.z_coordinate]
+            )
+        self.update_midpoint()
+
+    def get_rotation_matrix(self, angle, axis):
+        angle_cos = np.cos(angle)
+        angle_sin = np.sin(angle)
+        if axis == "x":
+            # X rotate
+            # x' = x
+            # y' = y * cos - z sin
+            # z' = y * sin + z cos
+            return np.array([[1, 0, 0], [0, angle_cos, -angle_sin], [0, angle_sin, angle_cos]])
+        elif axis == "y":
+            # Y rotate
+            # x' = x * cos + z * sin
+            # y' = y
+            # z' = -x * sin + z * cos
+            return np.array([[angle_cos, 0, angle_sin], [0, 1, 0], [-angle_sin, 0, angle_cos]])
+        elif axis == "z":
+            # Z rotate
+            # x' = x * cos - y * sin
+            # y' = x * sin + y cos
+            # z' = z
+            return np.array([[angle_cos, -angle_sin, 0], [angle_sin, angle_cos, 0], [0, 0, 1]])
+
+    def scale(self, scale_factor):
+        for dot in self.dots:
+            dot.x_coordinate = (
+                                       dot.x_coordinate - self.midpoint.x_coordinate
+            ) * scale_factor + self.midpoint.x_coordinate
+            dot.y_coordinate = (
+                                       dot.y_coordinate - self.midpoint.y_coordinate
+            ) * scale_factor + self.midpoint.y_coordinate
+            dot.z_coordinate = (
+                                       dot.z_coordinate - self.midpoint.z_coordinate
+            ) * scale_factor + self.midpoint.z_coordinate
+        self.update_midpoint()
+
+
+class Shapes(NamedTuple):
+    name: str
+    value: Figure
+
+
+class GraphicsEditor3D:
+    def __init__(self, root):
+        self.root = root
+        root.config(bg="blue")
+        self.root.title("3D Graphic Editor")
+
+        self.fig = plt.figure(figsize=(7, 7))
+        self.ax = self.fig.add_subplot(111, projection="3d")
+        self.ax.axis("on")
+
+        self.ax.set_xlim(-200, 200)
+        self.ax.set_ylim(-200, 200)
+        self.ax.set_zlim(-200, 200)
+
+        self.canvas = FigureCanvasTkAgg(self.fig, master=root)
+        self.canvas.get_tk_widget().grid(row=0, column=0, rowspan=4)
+
+        self.dots = []
+        self.shapes = []
+
+        self.selected_figure = None
+
+        self.preview_dot = Dot(x=0, y=0, z=0)
+
+        self.dot_frame = tk.Frame(self.root)
+        self.dot_frame.configure(bg="yellow")
+        self.dot_frame.grid(row=5, column=0, padx=10, pady=10)
+
+        self.x_label = tk.Label(self.dot_frame, text="X:")
+        self.x_label.grid(row=0, column=0, padx=5, pady=5)
+        self.x_entry = tk.Entry(self.dot_frame, width=10)
+        self.x_entry.grid(row=0, column=1, padx=5, pady=5)
+        self.x_entry.bind("<KeyRelease>", self.x_changed)
+
+        self.y_label = tk.Label(self.dot_frame, text="Y:")
+        self.y_label.grid(row=0, column=2, padx=5, pady=5)
+        self.y_entry = tk.Entry(self.dot_frame, width=10)
+        self.y_entry.grid(row=0, column=3, padx=5, pady=5)
+        self.y_entry.bind("<KeyRelease>", self.y_changed)
+
+        self.z_label = tk.Label(self.dot_frame, text="Z:")
+        self.z_label.grid(row=2, column=0, padx=5, pady=5)
+        self.z_entry = tk.Entry(self.dot_frame, width=10)
+        self.z_entry.grid(row=2, column=1, padx=5, pady=5)
+        self.z_entry.bind("<KeyRelease>", self.z_changed)
+
+        self.add_button = tk.Button(
+            self.dot_frame, text="Add Dot", command=self.add_dot
+        )
+        self.add_button.grid(row=2, column=2, columnspan=2, padx=5, pady=5)
+
+        self.create_figure_button = tk.Button(
+            self.dot_frame, text="Create Figure", command=self.create_figure
+        )
+        self.create_figure_button.grid(row=3, column=1, columnspan=2, padx=5, pady=5)
+
+        self.clear_button = tk.Button(
+            self.dot_frame, text="Clear", command=self.clear_all
+        )
+        self.clear_button.grid(row=3, column=2, columnspan=2, padx=5, pady=5)
+
+        self.shapes_combobox = ttk.Combobox(
+            self.root, values=self.shapes, state="readonly"
+        )
+        self.shapes_combobox.grid(row=4, column=0, padx=10, pady=10)
+        self.shapes_combobox.bind("<<ComboboxSelected>>", self.select_figure)
+
+        self.root.bind("<KeyPress-x>", self.move_figure_x_plus)
+        self.root.bind("<Control-x>", self.move_figure_x_minus)
+        self.root.bind("<KeyPress-y>", self.move_figure_y_plus)
+        self.root.bind("<Control-y>", self.move_figure_y_minus)
+        self.root.bind("<KeyPress-z>", self.move_figure_z_plus)
+        self.root.bind("<Control-z>", self.move_figure_z_minus)
+
+        self.root.bind("<Alt-x>", self.rotate_selected_figure_x)
+        self.root.bind("<Alt-y>", self.rotate_selected_figure_y)
+        self.root.bind("<Alt-z>", self.rotate_selected_figure_z)
+
+        self.root.bind("<KeyPress-i>", self.scale_selected_figure_plus_ten_percent)
+        self.root.bind("<KeyPress-r>", self.scale_selected_figure_minus_ten_percent)
+
+        self.file_menu = tk.Menu(root, tearoff=False)
+        self.file_menu.add_command(label="Open", command=self.open_file)
+        self.file_menu.add_command(label="Save", command=self.save_file)
+        root.config(menu=self.file_menu)
+
+    def _select_mode(self, mode: str) -> None:
+        self.selected_mode = mode
+
+    def save_file(self):
+        if self.shapes:
+            with open("figures.txt", "w") as file:
+                for figure in self.shapes:
+                    dots_str = " ".join(
+                        f"{dot.x_coordinate},{dot.y_coordinate},{dot.z_coordinate}"
+                        for dot in figure.value.dots
+                    )
+                    file.write(
+                        f"{figure.name} " f"{len(figure.value.dots)} {dots_str}\n"
+                    )
+
+    def open_file(self):
+        self.clear_all()
+        try:
+            with open("figures.txt", "r") as file:
+                for line in file:
+                    name, num_dots, *dots_str = line.strip().split(" ")
+                    num_dots = int(num_dots)
+                    dots = [Dot(*map(float, dot.split(","))) for dot in dots_str]
+                    self.shapes.append(Shapes(name=name, value=Figure(dots)))
+                    if not self.shapes_combobox["values"]:
+                        self.shapes_combobox["values"] = (name,)
+                    else:
+                        self.shapes_combobox["values"] += (name,)
+            self.update_plot()
+        except FileNotFoundError:
+            print("Error: File not found")
+
+    def scale_selected_figure_plus_ten_percent(self, *args):
+        if self.selected_figure:
+            self.selected_figure.scale(1.1)
+            self.update_plot()
+
+    def scale_selected_figure_minus_ten_percent(self, *args):
+        if self.selected_figure:
+            self.selected_figure.scale(0.9)
+            self.update_plot()
+
+    def rotate_selected_figure_x(self, *args):
+        if self.selected_figure:
+            self.selected_figure.rotate(np.pi / 18., "x")
+            self.update_plot()
+
+    def rotate_selected_figure_y(self, *args):
+        if self.selected_figure:
+            self.selected_figure.rotate(np.pi / 18., "y")
+            self.update_plot()
+
+    def rotate_selected_figure_z(self, *args):
+        if self.selected_figure:
+            self.selected_figure.rotate(np.pi / 18., "z")
+            self.update_plot()
+
+    def move_figure_x_plus(self, *args):
+        if self.selected_figure:
+            for dot in self.selected_figure.dots:
+                dot.x_coordinate += 1
+            self.update_plot()
+
+    def move_figure_x_minus(self, *args):
+        if self.selected_figure:
+            for dot in self.selected_figure.dots:
+                dot.x_coordinate -= 1
+            self.update_plot()
+
+    def move_figure_y_plus(self, *args):
+        if self.selected_figure:
+            for dot in self.selected_figure.dots:
+                dot.y_coordinate += 1
+            self.update_plot()
+
+    def move_figure_y_minus(self, *args):
+        if self.selected_figure:
+            for dot in self.selected_figure.dots:
+                dot.y_coordinate -= 1
+            self.update_plot()
+
+    def move_figure_z_plus(self, *args):
+        if self.selected_figure:
+            for dot in self.selected_figure.dots:
+                dot.z_coordinate += 1
+            self.update_plot()
+
+    def move_figure_z_minus(self, *args):
+        if self.selected_figure:
+            for dot in self.selected_figure.dots:
+                dot.z_coordinate -= 1
+            self.update_plot()
+
+    def select_figure(self, event):
+        selected_value = self.shapes_combobox.get()
+        self.selected_figure = next(
+            filter(lambda figure: figure.name == selected_value, self.shapes)
+        ).value
+        self.update_plot()
+
+    def clear_all(self):
+        self.dots = []
+        self.selection_dots = []
+        self.selected_figure = None
+        self.shapes = []
+        self.update_plot()
+
+    def x_changed(self, *args):
+        try:
+            self.preview_dot.x_coordinate = float(self.x_entry.get())
+        except ValueError:
+            return
+
+        self.update_plot()
+
+    def y_changed(self, *args):
+        try:
+            self.preview_dot.y_coordinate = float(self.y_entry.get())
+        except ValueError:
+            return
+
+        self.update_plot()
+
+    def z_changed(self, *args):
+        try:
+            self.preview_dot.z_coordinate = float(self.z_entry.get())
+        except ValueError:
+            return
+
+        self.update_plot()
+
+    def add_dot(self):
+        try:
+            x = float(self.x_entry.get())
+            y = float(self.y_entry.get())
+            z = float(self.z_entry.get())
+        except ValueError:
+            return
+
+        dot = Dot(x, y, z)
+        self.dots.append(dot)
+        self.update_plot()
+
+    def draw_figure(self, x_values, y_values, z_values):
+        num_steps = 20
+        lines = []
+        for i in range(len(x_values)):
+            for j in range(i + 1, len(x_values)):
+                lines.append([i, j])
+        interpolated_lines = []
+        for line in lines:
+            x_interp = np.linspace(x_values[line[0]], x_values[line[1]], num_steps)
+            y_interp = np.linspace(y_values[line[0]], y_values[line[1]], num_steps)
+            z_interp = np.linspace(z_values[line[0]], z_values[line[1]], num_steps)
+            interpolated_lines.append(list(zip(x_interp, y_interp, z_interp)))
+        for line in interpolated_lines:
+            x, y, z = zip(*line)
+            self.ax.plot(x, y, z, color="black", linewidth=1)
+
+    def create_figure(self):
+        figure = Figure(self.dots)
+        name = str(randint(1, 10000000))
+
+        figure_tuple = Shapes(name=name, value=figure)
+        self.shapes.append(figure_tuple)
+        if not self.shapes_combobox["values"]:
+            self.shapes_combobox["values"] = (name,)
+        else:
+            self.shapes_combobox["values"] += (name,)
+        self.dots = []
+        self.update_plot()
+
+    def update_plot(self):
+        self.ax.clear()
+
+        for figure in self.shapes:
+            dots = figure.value.dots
+            x_values = [dot.x_coordinate for dot in dots]
+            y_values = [dot.y_coordinate for dot in dots]
+            z_values = [dot.z_coordinate for dot in dots]
+            self.draw_figure(x_values, y_values, z_values)
+
+        if self.dots:
+            marked_x = [dot.x_coordinate for dot in self.dots]
+            marked_y = [dot.y_coordinate for dot in self.dots]
+            marked_z = [dot.z_coordinate for dot in self.dots]
+            self.ax.scatter(marked_x, marked_y, marked_z, color="red")
+
+        if self.preview_dot:
+            self.ax.scatter(
+                self.preview_dot.x_coordinate,
+                self.preview_dot.y_coordinate,
+                self.preview_dot.z_coordinate,
+                color="green",
+            )
+
+        if self.selected_figure:
+            self.selected_figure.update_midpoint()
+            for dot in self.selected_figure.dots:
+                self.ax.scatter(dot.x_coordinate, dot.y_coordinate, dot.z_coordinate, color="cyan")
+            self.ax.scatter(
+                self.selected_figure.midpoint.x_coordinate,
+                self.selected_figure.midpoint.y_coordinate,
+                self.selected_figure.midpoint.z_coordinate,
+                color="purple",
+            )
+
+        self.ax.set_xlim(-200, 200)
+        self.ax.set_ylim(-200, 200)
+        self.ax.set_zlim(-200, 200)
+        self.canvas.draw()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    editor = GraphicsEditor3D(root)
+    root.mainloop()
